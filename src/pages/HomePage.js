7877569c284a5d5cd0e2e16ee6a21e674605b826
mubarak <mubbyre6xx@gmail.com>
2022-07-31 19:02:10 +0100

    test

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -34,7 +34,7 @@ const HomePage = () => {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="Work sans">
-          CHATTY
+          CHATTYYY
         </Text>
       </Box>
       <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
